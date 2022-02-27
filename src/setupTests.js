859e01879c2a8c2d8080035561aed7dcdac27def
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-27 17:26:31 +0100

    added configurations for tests in setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,23 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+import { render } from "@testing-library/react";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import { server } from "./redux/mocks/server";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+const renderWithProviders = (component) => {
+  const Providers = ({ children }) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return render(component, { wrapper: Providers });
+};
+
+export default renderWithProviders;
