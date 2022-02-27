e872663db738b9918bb4f4901f9c51235bae08ac
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-27 17:28:40 +0100

    created RegisterForm.js

---
+++ src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { userRegisterThunk } from "../../redux/thunks/usersThunks";
+
+const RegisterForm = () => {
+  const dispatch = useDispatch();
+  const initialData = {
+    name: "",
+    username: "",
+    password: "",
+  };
+
+  const [formData, setFormData] = useState(initialData);
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dispatch(userRegisterThunk(formData));
+  };
+
+  return (
+    <>
+      <section className="container mb-3">
+        <form
+          className="mb-3"
+          onSubmit={onSubmit}
+          autoComplete="off"
+          noValidate
+        >
+          <div className="mb-3">
+            <label htmlFor="inputName" className="form-label">
+              Name
+            </label>
+            <input
+              name="name"
+              type="text"
+              className="form-control"
+              id="inputName"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="inputUsername" className="form-label">
+              Username
+            </label>
+            <input
+              name="username"
+              type="text"
+              className="form-control"
+              id="inputUsername"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="inputPassword" className="form-label">
+              Password
+            </label>
+            <input
+              name="password"
+              type="password"
+              className="form-control"
+              id="inputPassword"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Sign up
+          </button>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default RegisterForm;
