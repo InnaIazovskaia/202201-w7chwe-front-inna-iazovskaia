5b57b7a4f718facb0ecefe449bfcf09c1a494fa5
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-01 10:42:18 +0100

    modified userRegisterThunk in usersThunks.js

--- src/redux/thunks/usersThunks.js
+++ src/redux/thunks/usersThunks.js
@@ -1,4 +1,8 @@
-import { userRegisterAction } from "../actions/actionsCreators";
+import {
+  userLoginAction,
+  userRegisterAction,
+} from "../actions/actionsCreators";
+import jwtDecode from "jwt-decode";
 
 export const userRegisterThunk = (user) => async (dispatch) => {
   const response = await fetch(
@@ -14,5 +18,26 @@ export const userRegisterThunk = (user) => async (dispatch) => {
   if (response.ok) {
     const newUser = await response.json();
     dispatch(userRegisterAction(newUser));
+  }
+};
+
+export const userLoginThunk = (user) => async (dispatch) => {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_SOCIAL}users/login`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }
+  );
+
+  if (response.ok) {
+    const token = await response.json();
+    const { username } = await jwtDecode(token.token);
+    localStorage.setItem("UserToken", token.token);
+
+    dispatch(userLoginAction({ username, token: token.token }));
   }
 };
